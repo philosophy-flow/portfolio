5bfdbd7ba61221216def6c05b3c36e9379c99348
Jacob S <jacob.g.stewart@gmail.com>
2021-04-26 16:41:36 -0400

    changing about text

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -19,7 +19,7 @@ function About() {
         <div className="about-text-container">
           <h2 className="about-header">About Me</h2>
           <p className="about-body">
-            It’s exciting to live in a world of constantly evolving technology, and it's even more exciting to build things in it. My name is <span className="name">Jacob Stewart</span>, and I am a front end web developer that specializes in React. I am passionate about technology and learning new things, and I look forward to applying my knowledge in a professional environment while continuing my perpetual learning journey.
+            It’s exciting to live in a world of constantly evolving technology, and it's even more exciting to build things in it. My name is <span className="name">Jacob Stewart</span>, and I am a front end web developer that specializes in the React ecosystem. I am passionate about technology and learning new things, and I look forward to applying my knowledge in a professional environment while continuing my perpetual learning journey.
           </p>
           <div className="about-button-container">
             <Link className="about-nav-btn" to="/projects">see my work </Link>
