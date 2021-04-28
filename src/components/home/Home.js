a5104b256a2c9257daff79f40d568bd48a805a7d
Jacob S <jacob.g.stewart@gmail.com>
2021-04-28 12:04:55 -0400

    testing animated home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,27 @@
 import './Home.css';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
+import subwayBG from '../../assets/subway.jpg';
+import cityBG from '../../assets/test.jpg';
 
 import {motion} from "framer-motion";
 
+
 function Home() {
+  const [activeBG, setActiveBG] = useState(subwayBG)
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (activeBG === subwayBG) {
+        setActiveBG(cityBG)
+      } else if (cityBG) {
+        setActiveBG(subwayBG);
+      }
+    }, 4000);
+
+    return () => clearInterval(intervalId);
+  });
+
   return (
     <>
       <motion.section
@@ -12,8 +30,11 @@ function Home() {
         animate={{opacity: 1, y: 0}}
         exit={{opacity: 0, y: 25}}
         transition={{duration: .5}}
+        style={{backgroundImage: `url(${activeBG})`}}
       >
-        <div className="home-content-container">
+        <div
+          className="home-content-container"
+          >
           <h1 className="home-text">Hi. I'm Jacob, an NYC based web developer.</h1>
         </div>
       </motion.section>
