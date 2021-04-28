440317ed80ddfdfa71a12465133048dee85792b2
Jacob S <jacob.g.stewart@gmail.com>
2021-04-28 12:09:37 -0400

    removing animated bg

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,27 +1,10 @@
 import './Home.css';
-import React, {useState, useEffect} from 'react';
-
-import subwayBG from '../../assets/subway.jpg';
-import cityBG from '../../assets/test.jpg';
+import React from 'react';
 
 import {motion} from "framer-motion";
 
 
 function Home() {
-  const [activeBG, setActiveBG] = useState(subwayBG)
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      if (activeBG === subwayBG) {
-        setActiveBG(cityBG)
-      } else if (cityBG) {
-        setActiveBG(subwayBG);
-      }
-    }, 4000);
-
-    return () => clearInterval(intervalId);
-  });
-
   return (
     <>
       <motion.section
@@ -30,7 +13,6 @@ function Home() {
         animate={{opacity: 1, y: 0}}
         exit={{opacity: 0, y: 25}}
         transition={{duration: .5}}
-        style={{backgroundImage: `url(${activeBG})`}}
       >
         <div
           className="home-content-container"
